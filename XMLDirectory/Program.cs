b5b67090cb3e9ec750e2ffe7eca3045aee2839fb
kennethnorman <norman.kenneth@gmail.com>
2014-02-05 17:18:18 +0000

    Initial version

---
+++ XMLDirectory/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace XMLDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("XMLDirectory\n");
+
+            String dirPath = ".";
+            String outputFile = "XMLDirectory.xml";
+
+            if (args.Length == 1)
+            {
+                outputFile = args[1];
+            }
+
+
+            String extension = ".pdf";
+            String pattern = "*" + extension;
+            String ignore = "_private";
+            
+            // open an output file
+            StreamWriter OutputFile = new StreamWriter(@outputFile, false);
+            try
+            {
+                // Write the XML Header
+                OutputFile.WriteLine("<?xml version=\"1.0\"?>");
+                OutputFile.WriteLine("<SongList>");
+
+                var files = from file in Directory.EnumerateFiles(dirPath, pattern, SearchOption.AllDirectories)
+                            select new
+                            {
+                                File = file,
+                            };
+
+                List<KeyValuePair<String, String>> dnList = new List<KeyValuePair<String, String>>();
+                foreach (var f in files)
+                {
+                    String artist, track;
+
+                    string[] artisttrack = f.File.Split('\\');
+                    if (artisttrack.Length == 3)
+                    {
+                        artist = artisttrack[1];
+                        track = artisttrack[2];
+                        track = track.Replace("_", " ");
+                        track = track.Replace(artist, "");
+                        track = track.Replace(extension, "");
+                        track= track.Trim();
+                        if (track.Contains(ignore))
+                        {
+                            continue;
+                        }
+
+                        dnList.Add(new KeyValuePair<String, String>(artist, track));
+                        Console.WriteLine("{0}", f.File);
+                    }
+                }
+                Console.WriteLine("{0} files found.", files.Count().ToString());
+
+                foreach (var song in dnList)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append("    <Song>\r\n");
+                    sb.Append("        <Artist>" + song.Key + "</Artist>\r\n");
+                    sb.Append("        <Track>" + song.Value + "</Track>\r\n");
+                    sb.Append("    </Song>\r\n");
+                    OutputFile.Write(sb);
+                }
+                OutputFile.WriteLine("</SongList>");
+
+            }
+            catch (UnauthorizedAccessException UAEx)
+            {
+                Console.WriteLine(UAEx.Message);
+            }
+            catch (PathTooLongException PathEx)
+            {
+                Console.WriteLine(PathEx.Message);
+            }
+            finally
+            {
+                OutputFile.Close();
+            }
+
+            System.Console.WriteLine("Finished");
+        }
+    }
+}
