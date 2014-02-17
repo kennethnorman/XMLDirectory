a866177e7cd90a0e4f00f3b459046d7b56c3d0d8
kennethnorman <norman.kenneth@gmail.com>
2014-02-17 17:36:37 +0000

    Add songinfo extension

--- XMLDirectory/Program.cs
+++ XMLDirectory/Program.cs
@@ -14,81 +14,44 @@ namespace XMLDirectory
         {
             System.Console.WriteLine("XMLDirectory\n");
 
-            String dirPath = ".";
+            String command = "";
             String outputFile = "XMLDirectory.xml";
 
-            if (args.Length == 1)
+            if (args.Length >= 1)
             {
-                outputFile = args[0];
+                command = args[0];
             }
 
+            if (args.Length >= 2)
+            {
+                outputFile = args[1];
+            }
 
-            String extension = ".pdf";
-            String pattern = "*" + extension;
-            String ignore = "_private";
-            
+            command= command.ToLower();
+
             // open an output file
-            StreamWriter OutputFile = new StreamWriter(@outputFile, false);
+            StreamWriter OutputFile = null;
             try
             {
+                OutputFile = new StreamWriter(@outputFile, false);
                 // Write the XML Header
-                OutputFile.WriteLine("<?xml version=\"1.0\"?>");
-                OutputFile.WriteLine("<SongList>");
-
-                var files = from file in Directory.EnumerateFiles(dirPath, pattern, SearchOption.AllDirectories)
-                            select new
-                            {
-                                File = file,
-                            };
-
-                List<KeyValuePair<String, String>> dnList = new List<KeyValuePair<String, String>>();
-                foreach (var f in files)
+                OutputFile.WriteLine("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+                
+                switch (command)
                 {
-                    String artist, track;
-
-                    string[] artisttrack = f.File.Split('\\');
-                    if (artisttrack.Length == 3)
-                    {
-                        artist = artisttrack[1];
-                        track = artisttrack[2];
-
-                        if (track.Contains(ignore))
+                    case "songlist":
                         {
-                            continue;
+                            SongList(ref OutputFile);
                         }
-
-                        track = track.Replace("_", " ");
-                        track = track.Replace(artist, "");
-                        track = track.Replace(extension, "");
-                        track = track.Trim();
-
-                        artist = artist.Replace("&&", "&amp;");
-                        artist = artist.Replace("\"", "&quot;");
-                        artist = artist.Replace("<", "&lt;");
-                        artist = artist.Replace(">", "&gt;");
-
-                        track = track.Replace("&", "&amp;");
-                        track = track.Replace("\"", "&quot;");
-                        track = track.Replace("<", "&lt;");
-                        track = track.Replace(">", "&gt;");
-
-                        dnList.Add(new KeyValuePair<String, String>(artist, track));
-                        Console.WriteLine("{0}", f.File);
-                    }
+                        break;
+                    case "songinfo":
+                        {
+                            SongInfo(ref OutputFile);
+                        }
+                        break;
+                    default:
+                        break;
                 }
-                Console.WriteLine("{0} files found.", files.Count().ToString());
-
-                foreach (var song in dnList)
-                {
-                    StringBuilder sb = new StringBuilder();
-                    sb.Append("    <Song>\r\n");
-                    sb.Append("        <Artist>" + song.Key + "</Artist>\r\n");
-                    sb.Append("        <Track>" + song.Value + "</Track>\r\n");
-                    sb.Append("    </Song>\r\n");
-                    OutputFile.Write(sb);
-                }
-                OutputFile.WriteLine("</SongList>");
-
             }
             catch (UnauthorizedAccessException UAEx)
             {
@@ -104,6 +67,165 @@ namespace XMLDirectory
             }
 
             System.Console.WriteLine("Finished");
+        }
+
+        static void SongList(ref StreamWriter OutputFile)
+        {
+            String dirPath = ".";
+            String extension = ".pdf";
+            String pattern = "*" + extension;
+            String ignore = "_private";
+
+            OutputFile.WriteLine("<SongList>");
+
+            var files = from file in Directory.EnumerateFiles(dirPath, pattern, SearchOption.AllDirectories)
+                        select new
+                        {
+                            File = file,
+                        };
+
+            List<KeyValuePair<String, String>> dnList = new List<KeyValuePair<String, String>>();
+            foreach (var f in files)
+            {
+                String artist, track;
+
+                string[] artisttrack = f.File.Split('\\');
+                if (artisttrack.Length == 3)
+                {
+                    artist = artisttrack[1];
+                    track = artisttrack[2];
+
+                    if (track.Contains(ignore))
+                    {
+                        continue;
+                    }
+
+                    track = track.Replace("_", " ");
+                    track = track.Replace(artist, "");
+                    track = track.Replace(extension, "");
+                    track = track.Trim();
+
+                    artist = artist.Replace("&&", "&amp;");
+                    artist = artist.Replace("\"", "&quot;");
+                    artist = artist.Replace("<", "&lt;");
+                    artist = artist.Replace(">", "&gt;");
+
+                    track = track.Replace("&", "&amp;");
+                    track = track.Replace("\"", "&quot;");
+                    track = track.Replace("<", "&lt;");
+                    track = track.Replace(">", "&gt;");
+
+                    dnList.Add(new KeyValuePair<String, String>(artist, track));
+                    Console.WriteLine("{0}", f.File);
+                }
+            }
+            Console.WriteLine("{0} files found.", files.Count().ToString());
+
+            foreach (var song in dnList)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("    <Song>\r\n");
+                sb.Append("        <Artist>" + song.Key + "</Artist>\r\n");
+                sb.Append("        <Track>" + song.Value + "</Track>\r\n");
+                sb.Append("    </Song>\r\n");
+                OutputFile.Write(sb);
+            }
+            OutputFile.WriteLine("</SongList>");
+        }
+
+
+        static void SongInfo(ref StreamWriter OutputFile)
+        {
+            String dirPath = ".";
+            String extension = ".info.xml";
+            String pattern = "*" + extension;
+            String ignore = "_private";
+
+            OutputFile.WriteLine("<SongInfo>");
+
+            var files = from file in Directory.EnumerateFiles(dirPath, pattern, SearchOption.AllDirectories)
+                        select new
+                        {
+                            File = file,
+                        };
+
+            List<KeyValuePair<String, String>> dnList = new List<KeyValuePair<String, String>>();
+            foreach (var f in files)
+            {
+                String artist, track;
+
+                string[] artisttrack = f.File.Split('\\');
+                if (artisttrack.Length == 3)
+                {
+                    artist = artisttrack[1];
+                    track = artisttrack[2];
+
+                    if (track.Contains(ignore))
+                    {
+                        continue;
+                    }
+
+                    track = track.Replace("_", " ");
+                    track = track.Replace(artist, "");
+                    track = track.Replace(extension, "");
+                    track = track.Trim();
+
+                    artist = artist.Replace("&&", "&amp;");
+                    artist = artist.Replace("\"", "&quot;");
+                    artist = artist.Replace("<", "&lt;");
+                    artist = artist.Replace(">", "&gt;");
+
+                    track = track.Replace("&", "&amp;");
+                    track = track.Replace("\"", "&quot;");
+                    track = track.Replace("<", "&lt;");
+                    track = track.Replace(">", "&gt;");
+
+                    StringBuilder xmlFrag = new StringBuilder();
+                    xmlFrag.Append("    <Song>\r\n");
+                    xmlFrag.Append("        <Artist>" + artist + "</Artist>\r\n");
+                    xmlFrag.Append("        <Track>" + track + "</Track>\r\n");
+
+                    StreamReader InputFile = null;
+                    try
+                    {
+                        // open the info file
+                        InputFile = new StreamReader(f.File);
+                        while (!InputFile.EndOfStream)
+                        {
+                            String sLine = InputFile.ReadLine();
+                            if (sLine.Length == 0 || sLine.Contains("?xml"))
+                                continue;
+                            xmlFrag.Append("        " + sLine + "\r\n");
+                        }
+                    }
+                    catch (UnauthorizedAccessException UAEx)
+                    {
+                        Console.WriteLine(UAEx.Message);
+                    }
+                    catch (PathTooLongException PathEx)
+                    {
+                        Console.WriteLine(PathEx.Message);
+                    }
+                    finally
+                    {
+                        InputFile.Close();
+                    }
+                    
+                    
+                    xmlFrag.Append("    </Song>\r\n");
+
+
+                    dnList.Add(new KeyValuePair<String, String>(artist, xmlFrag.ToString()));
+                    Console.WriteLine("{0}", f.File);
+                }
+            }
+            Console.WriteLine("{0} files found.", files.Count().ToString());
+
+            foreach (var entry in dnList)
+            {
+                OutputFile.Write(entry.Value);
+            }
+            OutputFile.WriteLine("</SongInfo>");
         }
     }
 }
