54d2f8739e59f3bb6530df414d0ec48367914f41
kennethnorman <norman.kenneth@gmail.com>
2015-06-12 14:53:22 +0100

    Add file

---
+++ XMLDirectoryTest/CommonTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XMLDirectoryTest
+{
+    class CommonTest
+    {
+        public static void CompareResult(StreamReader ExpectedResult, StreamReader ActualResult)
+        {
+            while (true)
+            {
+                if (ExpectedResult.Read() != ActualResult.Read())
+                {
+                    Assert.Fail();
+                    break;
+                }
+                if (ExpectedResult.EndOfStream && ActualResult.EndOfStream)
+                {
+                    break;
+                }
+                if (ExpectedResult.EndOfStream || ActualResult.EndOfStream)
+                {
+                    Assert.Fail();
+                    break;
+                }
+            }
+        }
+    }
+}
